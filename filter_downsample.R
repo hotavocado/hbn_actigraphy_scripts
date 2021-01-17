4a27b81484be7c9dc5808e27b28f9c6f28b9de2b
Mike Xiao <mikemxxiao@gmail.com>
2021-01-17 15:44:37 -0500

    init

---
+++ filter_downsample.R
@@ -0,0 +1,88 @@
+require(tidyverse)
+
+# filter_data: filter data based on %valid minutes per day threshold and minimum number of days that pass this threshold
+
+#example usage: Filter dataset to at least 3 days of 95% wear
+#data_3_95 <- filter_data(data, wear_data, 0.95, 3)
+
+#note: "wear_data" argument takes a dataframe for input and refers to "valid_wear.csv", included in our processed data downloads
+
+filter_data <- function(acc_data, wear_data, percent_wear, minimum_days) {
+  
+  wearSummary <- wear_data %>%
+    group_by(ID) %>%
+    summarise(day = length(day),
+              wear = mean(minute_wear))
+  
+  #number of subjects in the original dataset
+  print(paste0('Input data includes ', length(unique(wearSummary$ID)), ' subjects, ', 
+         'averaging ', round(mean(wearSummary$day), 2), ' days per subject and ',
+         round(mean(wearSummary$wear), 2), ' minutes of valid wear per day.'))
+  
+  #keep only days that have greater than percent_wear threshold
+  wear_data <- wear_data  %>%
+    filter(minute_wear > percent_wear * 1440)
+  
+  wearSummary <- wear_data %>%
+    group_by(ID) %>%
+    summarise(day = length(day),
+              wear = mean(minute_wear))
+  
+  #number of subjects in the dataset after filtering by minutes per day
+  print(paste0('Data that passed [>', percent_wear*100, '% valid wear per day] threshold includes ', length(unique(wearSummary$ID)), ' subjects, ', 
+         'averaging ', round(mean(wearSummary$day), 2), ' days per subject and ',
+         round(mean(wearSummary$wear), 2), ' minutes of valid wear per day.'))
+  
+  #keep only subjects with greater than minimum_days days of data after applying percent_wear threshold
+  validIDs <- filter(wearSummary, day > minimum_days)
+  validIDs <- unique(validIDs$ID)
+  
+  wear_data <- wear_data %>% filter(ID %in% validIDs)
+  
+  wearSummary <- wear_data %>%
+    group_by(ID) %>%
+    summarise(day = length(day),
+              wear = mean(minute_wear))
+  
+  #number of subjects in the dataset after filtering by minutes per day
+  print(paste0('Data that passed [>', minimum_days, ' days] of [>', percent_wear*100, '% valid wear per day] threshold includes ', length(unique(wearSummary$ID)), ' subjects, ', 
+         'averaging ', round(mean(wearSummary$day), 2), ' days per subject and ',
+         round(mean(wearSummary$wear), 2), ' minutes of valid wear per day.'))
+  
+  
+  validID_days <- paste0(wear_data$ID, '-', wear_data$day)
+  
+  filtered_data <- acc_data %>% 
+    mutate(ID_days = paste0(ID, '-', day)) %>%
+    filter(ID_days %in% validID_days) %>%
+    select(-ID_days)
+  
+  return(filtered_data)
+  
+}
+
+
+# downsample: downsample 1 minute epoch dataset into longer epochs by taking the mean over each epoch
+
+#example usage: Downsample the 1 minute epoch dataset to 30 minute epochs
+
+#data_30minute <- downsample(data_1minute, 30)
+
+downsample <- function(dataset, epoch) {
+  
+  print(paste0('Downsampling 60s epoch data into ', epoch, ' minute epochs.'))
+  
+  dataset <- dataset %>% 
+    mutate(
+      minute = minute - 1,
+      minute = floor(minute/epoch)) %>%
+    group_by(ID, day, minute) %>%
+    summarise(timestamp = timestamp[1],
+              ENMO = mean(ENMO, na.rm = T),
+              wear = min(wear)) %>%
+    mutate(minute = (minute + 1) * epoch)
+  
+  return(dataset)
+  
+}
+
---
+++ ggir1.parameter.R
@@ -0,0 +1,114 @@
+# GGIR version 2.0-0
+
+library(GGIR)
+
+
+
+################################################################################################################################
+ 
+myshell<-function(studyname,datadir,outputdir,mode=c(1,2,3,4,5),f0=0,f1=c(),desiredtz = "Etc/GMT+5"){ 
+ 
+g.shell.GGIR(#=======================================
+             # INPUT NEEDED:
+              mode=mode, 
+             datadir=datadir,
+             outputdir= outputdir,
+             f0=f0, f1=f1, 
+             studyname=studyname,
+             overwrite = FALSE,
+             idloc=1,
+             print.filename=TRUE,
+             storefolderstructure = FALSE,
+             dformat=2,
+             mon=3,
+             #-------------------------------
+             # Part 1:
+             #-------------------------------
+             # Key functions: reading file, auto-calibration, and extracting features
+             desiredtz = desiredtz,
+             do.enmo = TRUE,
+             do.anglez=TRUE,
+             do.hfen = FALSE,
+             windowsizes = c(5,900,3600), 
+             do.cal=TRUE,
+             chunksize=1,
+             printsummary=TRUE,
+
+			 #-------------------------------
+             # Part 2:
+             #-------------------------------
+             
+             strategy = 1,
+             hrs.del.start = 0,  # for strategy = 1
+             hrs.del.end = 0,
+             maxdur = 0,    # for strategy = 1, changed from NIMH script
+             includedaycrit = 16,
+             winhr = c(5,10),
+             epochvalues2csv=FALSE,
+             L5M5window = c(0,24),
+             M5L5res = 10,
+             qlevels = c(c(1380/1440),c(1410/1440)),
+             qwindow=c(0,24),
+             ilevels = c(seq(0,400,by=50),8000),
+             mvpathreshold =c(125),
+             do.imp=TRUE,
+
+             #-------------------------------
+             # Part 3:
+             # (detection of sustained inactivity periods as needed for sleep detection in g.part4)
+             #-------------------------------
+             # Key functions: Sleep detection
+             timethreshold= c(5),
+             anglethreshold=5,
+             ignorenonwear = TRUE,
+             #-------------------------------
+             # Part 4:
+             # \uff08Labels detected sustained inactivity periods by g.part3 as either nocturnal sleep or daytime sustained inactivity)
+             #-------------------------------
+             # Key functions: Integrating sleep log (if available) with sleep detection
+             # storing day and person specific summaries of sleep
+
+             excludefirstlast = FALSE,
+             includenightcrit = 16,
+             def.noc.sleep = c(1),
+             # loglocation= "~/Desktop/NIMH Family Study/sleeplog_final02202018_wide.csv",
+             # colid=1,
+             # coln1=2,
+             outliers.only = FALSE,
+             relyonsleeplog = FALSE,
+             sleeplogidnum = FALSE,
+             do.visual = TRUE,
+             criterror = 0,
+             nnights = 60, #changed from NIMH script
+
+             #-------------------------------
+             # Part 5:
+             #-------------------------------
+             # Key functions: merge output from physical activity and sleep analysis into one report
+             threshold.lig = c(30,40,50),
+             threshold.mod = c(100,120,125),
+             threshold.vig = c(400,500),
+             boutcriter = 0.8,
+             boutcriter.in = 0.9,
+             boutcriter.lig = 0.8,
+             boutcriter.mvpa = 0.8,
+             boutdur.in = c(10,20,30),
+             boutdur.lig = c(1,5, 10),
+             boutdur.mvpa = c(1,5,10),
+             timewindow = c("WW"),
+            # -----------------------------------
+            # Report generation
+            # -----------------------------------
+            # Key functions: Generating reports based on meta-data
+             #do.report=c(2,4,5),	
+             visualreport=TRUE,
+             dofirstpage = TRUE,
+             viewingwindow=1
+)
+
+
+}
+
+
+
+ 
